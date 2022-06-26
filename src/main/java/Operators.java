5690708dd47c2c824717e8cb1dc88e36704358b7
Mary <mary@Zipcoders-MacBook-Pro-9.local>
2022-06-25 21:20:03 -0400

    added addition, subtraction and multiplication

---
+++ src/main/java/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public Double Addition(double num1,double num2){
+        double solution;
+        solution = num1 + num2;
+        return solution;}
+
+    public Double Subtraction (double num1,double num2){
+        double solution;
+        solution = num1 - num2;
+        return solution;}
+
+    public Double Multiplication (double num1,double num2){
+        double solution;
+        solution = num1 * num2;
+        return solution;
+}
+}
