39c4f8894ad7196b0c4faa0c247581dd1a82e4da
Mary <mary@Zipcoders-MacBook-Pro-9.local>
2022-06-25 21:53:17 -0400

    Finished all operations

--- src/main/java/Operators.java
+++ src/main/java/Operators.java
@@ -1,17 +1,33 @@
+
+
+
+
+
+
 public class Operators {
-    public Double Addition(double num1,double num2){
+
+    public Operators(){
+
+    }
+    public Double Addition(double num1, double num2) {
         double solution;
         solution = num1 + num2;
-        return solution;}
+        return solution;
+    }
 
-    public Double Subtraction (double num1,double num2){
+    public Double Subtraction(double num1, double num2) {
         double solution;
         solution = num1 - num2;
-        return solution;}
+        return solution;
+    }
 
-    public Double Multiplication (double num1,double num2){
+    public Double Multiplication(double num1, double num2) {
         double solution;
         solution = num1 * num2;
         return solution;
-}
-}
+
+    }
+    public Double Division(double num1, double num2) {
+            double solution;
+            solution = num1 / num2;
+            return solution; } }
